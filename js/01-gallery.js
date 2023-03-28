ceb7cf7a41d3034779ade08382ff7f11a143c2b3
Heorhii Honcharov <0019@ukr.net>
2023-03-28 09:39:25 +0200

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let anInstance = {};
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createImageGallery(galleryItems);
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener("click", onClickGalleryItem);
+
+function onClickGalleryItem(event) {
+  event.preventDefault();
+
+  const isOnImageClick = event.target.classList.contains("gallery__image");
+  if (!isOnImageClick) {
+    return;
+  }
+
+  document.addEventListener("keydown", onESCpressed);
+  const getLargeImageUrl = event.target.dataset.source;
+  showFullImage(getLargeImageUrl);
+}
+
+function showFullImage(imageLargeUrl) {
+  anInstance = basicLightbox.create(
+    `
+  <img src="${imageLargeUrl}">
+  `,
+    {
+      onClose: removeOnEscPressed,
+    }
+  );
+  anInstance.show();
+}
+
+function removeOnEscPressed() {
+  document.removeEventListener("keydown", onESCpressed);
+}
+
+function onESCpressed(event) {
+  if (event.code == "Escape") {
+    anInstance.close();
+    removeOnEscPressed();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createImageGallery(galleryItems);
+
+function createImageGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `;
+    })
+    .join("");
+}
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener("click", onClickGalleryItem);
+
+let imgGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  showCounter: false,
+});
+
+function onClickGalleryItem(e) {
+  imgGallery.on("show.simplelightbox");
+  imgGallery.on("error.simplelightbox", function (e) {
+    console.log("error in SimpleGallery =>", e);
+  });
+}
